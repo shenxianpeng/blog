2ad2666ad21b247552b6e1035e56b71ab6ccfafc
shenxianpeng <xianpeng.shen@gmail.com>
2020-12-25 16:23:27 +0800

    update deploy.sh and readme.md

--- deploy.sh
+++ deploy.sh
@@ -8,20 +8,53 @@ _check_result()
     fi
 }
 
-hexo server > /dev/null 2>&1 &
-sleep 20 && curl -Is http://localhost:4000/ | head -n 1
-_check_result
+check_server()
+{
+    hexo server > /dev/null 2>&1 &
+    sleep 20 && curl -Is http://localhost:4000/ | head -n 1
+    _check_result
+}
 
-hexo clean
-_check_result
+clean()
+{
+    hexo clean
+    _check_result
+}
 
-hexo generate
-_check_result
+generate()
+{
+    hexo generate
+    _check_result
+}
+
+copy_readme()
+{
+    if [ -d public/ ];then
+        rm -rf public/README.*
+        cp source/README.md public/
+        _check_result
+    fi
+}
+
+deploy(){
+    hexo deploy
+    _check_result
+}
+
+########################
+# Update Blog Websites #
+########################
+check_server
+clean
+generate
+copy_readme
+deploy
+
+exit 0
 
 
-if [ -d public/ ];then
-    cp source/README.md public/
-fi
 
-hexo deploy
-_check_result
+
+
+
+
